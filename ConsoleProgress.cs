bf6bf6de18447dba5452f2825f1f1a2c1b8f8a13
k.zmorzynski <k.zmorzynski@intsol.pl>
2025-08-07 14:11:34 +0200

    added timeout for getting manifest; temporarily (?) changed to local YoutubeExplode.dll due to not handling 403 manifest errors (https://github.com/Tyrrrz/YoutubeExplode/pull/901); added versioning; added simple console progress bar

---
+++ ConsoleProgress.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SynoCastNET
+{
+    // from YoutubeExplode
+    internal class ConsolePercentProgress(TextWriter writer) : IProgress<double>, IDisposable
+    {
+        private readonly int _posX = Console.CursorLeft;
+        private readonly int _posY = Console.CursorTop;
+
+        private int _lastLength;
+
+        public ConsolePercentProgress()
+            : this(Console.Out) { }
+
+        private void EraseLast()
+        {
+            if (_lastLength > 0)
+            {
+                Console.SetCursorPosition(_posX, _posY);
+                writer.Write(new string(' ', _lastLength));
+                Console.SetCursorPosition(_posX, _posY);
+            }
+        }
+
+        private void Write(string text)
+        {
+            EraseLast();
+            writer.Write(text);
+            _lastLength = text.Length;
+        }
+
+        public void Report(double progress) => Write($"{progress:P1}");
+
+        public void Dispose() => EraseLast();
+    }
+
+    internal class ConsoleStarProgress(TextWriter writer) : IProgress<double>, IDisposable
+    {
+        private int _lastProgress;
+
+        public ConsoleStarProgress()
+            : this(Console.Out) { }
+
+        public void Report(double progress)
+        {
+            // just add star after each 1 percent
+            int thisProgress = (int)Math.Round(progress * 100.0, MidpointRounding.AwayFromZero);
+            if(thisProgress != _lastProgress)
+            {
+                writer.Write("*");
+                _lastProgress = thisProgress;
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using SynoCastNET;
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
 using YoutubeExplode;
@@ -8,7 +9,9 @@ using YoutubeExplode.Playlists;
 using YoutubeExplode.Videos;
 using YoutubeExplode.Videos.Streams;
 
-Console.WriteLine("SynoCASTNET - Youtube audio only podcast downloader for Synology NAS");
+Console.WriteLine("SynoCASTNET - YouTube audio only podcast downloader for Synology NAS");
+var buildInfo = GetBuildInfo();
+Console.WriteLine($"Build version: {buildInfo.Version}, date (UTC): {buildInfo.BuildDateUTC}");
 
 var youtube = new YoutubeClient();
 
@@ -26,6 +29,7 @@ static async Task ProcessSourcesAsync(Config config, YoutubeClient youtube)
 {
     foreach (var source in config.sources)
     {
+        Console.WriteLine("\n***************************************************************");
         Console.WriteLine($"Processing source: {source.name} (Language: {source.language}, Container: {source.container ?? "any"})");
         IEnumerable<PlaylistVideo> videos = Enumerable.Empty<PlaylistVideo>();
         try
@@ -70,53 +74,69 @@ static async Task ProcessVideosAsync(IEnumerable<PlaylistVideo> videos, Config c
 
     foreach (var video in videos.Reverse()) // Reverse to process oldest first
     {
-        if(video.Duration is null)
+        if (video.Duration is null)
         {
             Console.WriteLine($"Video {video.Url} has no duration information (live?). Skipping.");
             continue;
         }
-        var v = await youtube.Videos.GetAsync(video.Url);
-        Console.WriteLine($"Title: {v.Title}");
-        Console.WriteLine($"Url: {video.Url}");
-        Console.WriteLine($"Duration: {video.Duration}");
-        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Url);
-        var audioStreams = streamManifest.GetAudioOnlyStreams();
-        var streamInfo = audioStreams
-                            .Where(s => string.Equals(s.AudioLanguage?.Code, source.language, StringComparison.OrdinalIgnoreCase)
-                                || (s.AudioLanguage is not null && s.AudioLanguage.Value.Code.StartsWith($"{source.language}-", StringComparison.OrdinalIgnoreCase)));
-        // check if we have any stream - if not then get streams for any language
-        if (!streamInfo.Any())
+
+        try
         {
-            Console.WriteLine($"No audio streams found for language '{source.language}'. Trying to get streams without language set.");
-            streamInfo = audioStreams.Where(s => s.AudioLanguage is null || string.IsNullOrWhiteSpace(s.AudioLanguage.Value.Code));
-        }
-        if (!string.IsNullOrWhiteSpace(source.container))
-            streamInfo = streamInfo.Where(s => string.Equals(s.Container.Name, source.container, StringComparison.OrdinalIgnoreCase));
-        
-        var bestStream = streamInfo.OrderByDescending(s => s.Bitrate).FirstOrDefault();
-        if (bestStream != null)
-        {
-            Console.WriteLine($"Best Stream: {bestStream.Container}, {bestStream.Bitrate} bps, {bestStream.Size} bytes, codec: {bestStream.AudioCodec}, lang: {bestStream.AudioLanguage} ({bestStream.IsAudioLanguageDefault})");
-            // Download the best stream
-            var safeTitle = string.Join("_", v.Title.Split(GetUniversalInvalidFileNameChars()));
-            var fileExt = bestStream.Container.Name;
-            var filePath = Path.Combine(finalDataDir, $"{safeTitle}.{fileExt}");
-            if (File.Exists(filePath))
+            using var manifestTimeout = new CancellationTokenSource();
+            manifestTimeout.CancelAfter(TimeSpan.FromSeconds(30)); // Set a timeout for each manifest retrieval
+
+            var v = await youtube.Videos.GetAsync(video.Url);
+            Console.WriteLine($"Title: {v.Title}");
+            Console.WriteLine($"Url: {v.Url}");
+            Console.WriteLine($"Duration: {v.Duration}");
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(v.Url, manifestTimeout.Token);
+            var audioStreams = streamManifest.GetAudioOnlyStreams();
+            var streamInfo = audioStreams
+                                .Where(s => string.Equals(s.AudioLanguage?.Code, source.language, StringComparison.OrdinalIgnoreCase)
+                                    || (s.AudioLanguage is not null && s.AudioLanguage.Value.Code.StartsWith($"{source.language}-", StringComparison.OrdinalIgnoreCase)));
+            // check if we have any stream - if not then get streams for any language
+            if (!streamInfo.Any())
             {
-                Console.WriteLine($"File already exists: {filePath}. Skipping download.");
+                Console.WriteLine($"No audio streams found for language '{source.language}'. Trying to get streams without language set.");
+                streamInfo = audioStreams.Where(s => s.AudioLanguage is null || string.IsNullOrWhiteSpace(s.AudioLanguage.Value.Code));
+            }
+            if (!string.IsNullOrWhiteSpace(source.container))
+                streamInfo = streamInfo.Where(s => string.Equals(s.Container.Name, source.container, StringComparison.OrdinalIgnoreCase));
+
+            var bestStream = streamInfo.OrderByDescending(s => s.Bitrate).FirstOrDefault();
+            if (bestStream != null)
+            {
+                Console.WriteLine($"Best Stream: {bestStream.Container}, {bestStream.Bitrate} bps, {bestStream.Size} bytes, codec: {bestStream.AudioCodec}, lang: {bestStream.AudioLanguage} ({bestStream.IsAudioLanguageDefault})");
+                // Download the best stream
+                var safeTitle = string.Join("_", v.Title.Split(GetUniversalInvalidFileNameChars()));
+                var fileExt = bestStream.Container.Name;
+                var filePath = Path.Combine(finalDataDir, $"{safeTitle}.{fileExt}");
+                if (File.Exists(filePath))
+                {
+                    Console.WriteLine($"File already exists: {filePath}. Skipping download.");
+                }
+                else
+                {
+                    Console.WriteLine($"Downloading to: {filePath}");
+                    using var progress = new ConsoleStarProgress();
+                    await youtube.Videos.Streams.DownloadAsync(bestStream, filePath, progress);
+                    Console.WriteLine("\nDownload complete.");
+                }
             }
             else
             {
-                Console.WriteLine($"Downloading to: {filePath}");
-                await youtube.Videos.Streams.DownloadAsync(bestStream, filePath);
-                Console.WriteLine("Download complete.");
+                Console.WriteLine($"No audio stream found for language '{source.language}' and container '{source.container ?? "any"}'.");
             }
+            Console.WriteLine();
         }
-        else
+        catch(OperationCanceledException)
         {
-            Console.WriteLine($"No audio stream found for language '{source.language}' and container '{source.container ?? "any"}'.");
+            Console.WriteLine($"Processing of video {video.Url} was cancelled due to timeout.");
         }
-        Console.WriteLine();
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error processing video {video.Url}: {ex.Message}");
+        }
     }
     // Remove oldest files if maxDownloads is set (or use maxItems*2 if null)
     int effectiveMaxDownloads = source.maxDownloads ?? (source.maxItems * 2);
@@ -143,4 +163,25 @@ static async Task ProcessVideosAsync(IEnumerable<PlaylistVideo> videos, Config c
             }
         }
     }
+}
+
+static (string Version, string BuildDateUTC) GetBuildInfo()
+{
+    var assembly = Assembly.GetExecutingAssembly();
+    const string BuildVersionMetadataPrefix = "+buildUTC";
+
+    var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+    if (attribute?.InformationalVersion != null)
+    {
+        var value = attribute.InformationalVersion;
+        var index = value.IndexOf(BuildVersionMetadataPrefix);
+        if (index > 0)
+        {
+            string version = value.Substring(0, index);
+            string buildDate = value.Substring(index + BuildVersionMetadataPrefix.Length);
+            return (version, buildDate);
+        }
+    }
+
+    return ("?", "?");
 }
